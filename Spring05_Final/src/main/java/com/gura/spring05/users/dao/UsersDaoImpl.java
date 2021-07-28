73cd100ed9f86da27326d488f5824bcd11513ac5
hs_keko <manic9075@gmail.com>
2021-07-28 16:13:06 +0900

    회원 가입 요청 처리

--- Spring05_Final/src/main/java/com/gura/spring05/users/dao/UsersDaoImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/dao/UsersDaoImpl.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.gura.spring05.users.dto.UsersDto;
+
 //bean 이 될 수 있도록
 @Repository
 public class UsersDaoImpl implements UsersDao {
@@ -11,4 +13,20 @@ public class UsersDaoImpl implements UsersDao {
 	//필요한 핵심 의존 객체
 	@Autowired
 	private SqlSession session;
+
+	@Override
+	public boolean isExist(String inputId) {
+		//인자로 전달 받은 아이디가 존재 하는지
+		String id=session.selectOne("users.isExist", inputId);
+		if(id==null) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+
+	@Override
+	public void insert(UsersDto dto) {
+		session.insert("users.insert", dto);
+	}
 }
