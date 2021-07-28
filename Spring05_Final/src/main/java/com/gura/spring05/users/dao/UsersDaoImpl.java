021502233e9bea8df1f1c6d59cbeb34f2ef652d6
hs_keko <manic9075@gmail.com>
2021-07-29 00:23:32 +0900

    수업내용 추가

--- Spring05_Final/src/main/java/com/gura/spring05/users/dao/UsersDaoImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/dao/UsersDaoImpl.java
@@ -16,7 +16,7 @@ public class UsersDaoImpl implements UsersDao {
 
 	@Override
 	public boolean isExist(String inputId) {
-		//인자로 전달 받은 아이디가 존재 하는지
+		//인자로 전달 받은 아이디가 존재 하는지 select 해 본다.
 		String id=session.selectOne("users.isExist", inputId);
 		if(id==null) {
 			return false;
@@ -28,5 +28,12 @@ public class UsersDaoImpl implements UsersDao {
 	@Override
 	public void insert(UsersDto dto) {
 		session.insert("users.insert", dto);
+	}
+
+	@Override
+	public UsersDto getData(String id) {
+		
+		// 존재하지 않은 아이디면 null 이 리턴된다.
+		return session.selectOne("users.getData", id);
 	}
 }
--- Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersService.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersService.java
@@ -2,10 +2,15 @@ package com.gura.spring05.users.service;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+
 import com.gura.spring05.users.dto.UsersDto;
 
 public interface UsersService {
 	
 	public Map<String, Object> isExistId(String inputId);
 	public void addUsers(UsersDto dto);
+	public void loginProcess(UsersDto dto, HttpSession session);
 }
