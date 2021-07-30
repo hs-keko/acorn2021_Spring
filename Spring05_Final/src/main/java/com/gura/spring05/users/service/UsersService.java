cfb2cd4ff55d6356923cde481b06e0547bd5d492
hs_keko <manic9075@gmail.com>
2021-07-30 11:55:03 +0900

    회원 탈퇴 요청 구현

--- Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersService.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersService.java
@@ -20,4 +20,5 @@ public interface UsersService {
 	public Map<String, Object> saveProfileImage(HttpServletRequest request,
 			MultipartFile mFile);
 	public void updateUser(UsersDto dto, HttpSession session);
+	public void deleteUser(HttpSession session, ModelAndView mView);
 }
