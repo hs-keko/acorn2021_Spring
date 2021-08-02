d5ee60faf04f2e6b95b30b776c4f743579d5d28f
hs_keko <manic9075@gmail.com>
2021-08-02 11:07:17 +0900

    File List 요청 구현

--- Spring05_Final/src/main/java/com/gura/spring05/file/dto/FileDto.java
+++ Spring05_Final/src/main/java/com/gura/spring05/file/dto/FileDto.java
@@ -4,18 +4,37 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class FileDto {
 	private int num;
+	private String writer;
 	private String title;
 	// 업로드되는 파일의 정보를 담을 MultipartFile type 도 필드에 선언한다.
 	// 필드명을 <input type="file" name="myFile" /> 에서 name 속성의 value 와 일치 시킨다.
 	private MultipartFile myFile;
+	//원본 파일명
+	private String orgFileName;
+	//파일 시스템에 저장된 파일명
+	private String saveFileName;
+	//파일의 크기 
+	private long fileSize;
+	private String regdate;
+	//페이징 처리를 위한 필드
+	private int startRowNum;
+	private int endRowNum; 
 	
 	public FileDto() {}
 
-	public FileDto(int num, String title, MultipartFile myFile) {
+	public FileDto(int num, String writer, String title, MultipartFile myFile, String orgFileName, String saveFileName,
+			long fileSize, String regdate, int startRowNum, int endRowNum) {
 		super();
 		this.num = num;
+		this.writer = writer;
 		this.title = title;
 		this.myFile = myFile;
+		this.orgFileName = orgFileName;
+		this.saveFileName = saveFileName;
+		this.fileSize = fileSize;
+		this.regdate = regdate;
+		this.startRowNum = startRowNum;
+		this.endRowNum = endRowNum;
 	}
 
 	public int getNum() {
@@ -24,6 +43,14 @@ public class FileDto {
 
 	public void setNum(int num) {
 		this.num = num;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
 	}
 
 	public String getTitle() {
@@ -41,6 +68,54 @@ public class FileDto {
 	public void setMyFile(MultipartFile myFile) {
 		this.myFile = myFile;
 	}
-	
+
+	public String getOrgFileName() {
+		return orgFileName;
+	}
+
+	public void setOrgFileName(String orgFileName) {
+		this.orgFileName = orgFileName;
+	}
+
+	public String getSaveFileName() {
+		return saveFileName;
+	}
+
+	public void setSaveFileName(String saveFileName) {
+		this.saveFileName = saveFileName;
+	}
+
+	public long getFileSize() {
+		return fileSize;
+	}
+
+	public void setFileSize(long fileSize) {
+		this.fileSize = fileSize;
+	}
+
+	public String getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
+	public int getStartRowNum() {
+		return startRowNum;
+	}
+
+	public void setStartRowNum(int startRowNum) {
+		this.startRowNum = startRowNum;
+	}
+
+	public int getEndRowNum() {
+		return endRowNum;
+	}
+
+	public void setEndRowNum(int endRowNum) {
+		this.endRowNum = endRowNum;
+	}
+
 	
 }
