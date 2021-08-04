37754cc7fcb861ddf585f89fbf9c90eb46f5f13a
hs_keko <manic9075@gmail.com>
2021-08-04 14:22:50 +0900

    Spring04 수정

--- Spring04_JavaBasic/src/test/main2/MainClass.java
+++ Spring04_JavaBasic/src/test/main2/MainClass.java
@@ -7,9 +7,9 @@ import test.mypac.Weapon;
 
 public class MainClass {
 	public static void main(String[] args) {
-		//무언가를 공격해야한다면 어떤 코딩이 필요할까?
+		//무언가를 공격해야 한다면 어떤 코딩이 필요할까?
 		
-		// init.xml 문서를 해석해서 spring bean container 의 개념을 만든다.
+		// init.xml 문서를 해석해서 spring bean container 의 개념을 만든다. 
 		ApplicationContext context=
 				new ClassPathXmlApplicationContext("test/main2/init.xml");
 		// spring bean container 에서 Weapon type 의 참조값을 얻어온다.
@@ -18,5 +18,4 @@ public class MainClass {
 		//목적 달성!
 		w.attack();
 	}
-
 }
--- Spring04_JavaBasic/src/test/mypac/WeaponImpl.java
+++ Spring04_JavaBasic/src/test/mypac/WeaponImpl.java
@@ -1,10 +1,10 @@
 package test.mypac;
 
-public class WeaponImpl implements Weapon {
+public class WeaponImpl implements Weapon{
 
 	@Override
 	public void attack() {
 		System.out.println("무언가를 공격해요!");
-		}
+	}
 
 }
